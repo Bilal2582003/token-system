2622a8901f4b91b5c13e3488bb62f6186e4270e8
huzaifa2582003 <huzaifa2582003@gamil.com>
2026-03-16 19:54:59 +0500

    modify for live

--- config/env.php
+++ config/env.php
@@ -5,8 +5,8 @@
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-define('DB_HOST', $_ENV['DB_HOST']);
-define('DB_NAME', $_ENV['DB_DATABASE']);
-define('DB_USER', $_ENV['DB_USERNAME']);
-define('DB_PASS', $_ENV['DB_PASSWORD']);
+define('DB_HOST', $_SERVER['SERVER_NAME'] == 'localhost' ? $_ENV['DB_HOST'] : $_ENV['LIVEDB_HOST']);
+define('DB_NAME', $_SERVER['SERVER_NAME'] == 'localhost' ? $_ENV['DB_DATABASE'] :  $_ENV['LIVEDB_DATABASE']);
+define('DB_USER', $_SERVER['SERVER_NAME'] == 'localhost' ? $_ENV['DB_USERNAME'] : $_ENV['LIVEDB_USERNAME']);
+define('DB_PASS', $_SERVER['SERVER_NAME'] == 'localhost' ? $_ENV['DB_PASSWORD'] : $_ENV['LIVEDB_PASSWORD']);
 define('SITE_URL', $_ENV['SITE_URL'] ?? 'http://localhost/astana-project');
